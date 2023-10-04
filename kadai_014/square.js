380010c316fe4623055a6fe7b620393d90a509c1
小松侑末 <yurakomatsu@komatsuyuumatsunoMacBook-Air.local>
2023-10-04 20:51:06 +0900

    課題14再提出

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,8 +1,12 @@
 const calculateSquare = (num) => {
     const result = num * num;
-    console.log(result); 
+    return result;
   };
   
-  calculateSquare(5); 
-  calculateSquare(8); 
+  // 関数を呼び出し、引数を指定
+  const inputValue = 5; // 例として5を渡す
+  const squareResult = calculateSquare(inputValue);
+  
+  // 計算結果をコンソールに出力
+  console.log(squareResult);
   
